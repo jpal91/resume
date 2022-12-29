5136297f42a73915a58e294a8a72bbf05517ffc2
jpal91 <pallanschja@gmail.com>
2022-12-29 07:35:16 -0500

    added additional text

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Grid from "@mui/material/Grid";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
+import Box from '@mui/material/Box'
 
 const Home = () => {
 	return (
@@ -10,8 +11,11 @@ const Home = () => {
 				<title>Resume</title>
 			</Head>
 			<Container sx={{ width: '100%' }}>
-				<Grid xs={12} sx={{ display: 'flex' }}>
-					<Typography variant='h1'>$ cat names.txt | grep $MY_NAME | echo</Typography>
+				<Grid xs={12} sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+					<Box>
+						<Typography variant='h1'>$ cat names.txt | grep $MY_NAME | echo</Typography>
+						<Typography variant='h1'>Hello, my name is Justin Pallansch</Typography>
+					</Box>
 				</Grid>
 			</Container>
 		</>
