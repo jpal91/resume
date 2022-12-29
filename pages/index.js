e423d0e5867d8ac502cd7656078f41bba407b93f
jpal91 <pallanschja@gmail.com>
2022-12-29 08:34:08 -0500

    trying out typed

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import React, { useState, useRef } from 'react'
+import Typed from 'react-typed'
 import Head from "next/head";
 import Grid from "@mui/material/Grid";
 import Container from "@mui/material/Container";
@@ -5,6 +7,9 @@ import Typography from "@mui/material/Typography";
 import Box from '@mui/material/Box'
 
 const Home = () => {
+	const [typeState, setTypeState] = useState(1)
+	let tRef = useRef()
+	
 	return (
 		<>
 			<Head>
@@ -12,9 +17,21 @@ const Home = () => {
 			</Head>
 			<Container sx={{ width: '100%' }}>
 				<Grid xs={12} sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-					<Box>
-						<Typography variant='h1'>$ cat names.txt | grep $MY_NAME | echo</Typography>
-						<Typography variant='h1'>Hello, my name is Justin Pallansch</Typography>
+					<Box sx={{ width: '100%' }}>
+						<Typography variant='h1'>
+							<span>$ </span>
+							<Typed 
+								// typedRef={(typed) => tRef = typed}
+								ref={tRef}
+								strings={[' cat names.txt | grep $MY_NAME | echo']}
+								typeSpeed={50}
+								onComplete={() => {
+									tRef.current.typed.showCursor = false
+									console.log(tRef)
+								}}
+							/>
+						</Typography>
+						<Typography variant='h1' sx={{ display: typeState < 2 ? 'none' : ''}}>$</Typography>
 					</Box>
 				</Grid>
 			</Container>
