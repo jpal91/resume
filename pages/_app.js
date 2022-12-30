b1edf84be20689c91274f2ee388c19668a44250f
jpal91 <pallanschja@gmail.com>
2022-12-30 09:28:35 -0500

    added redux and devtools

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,26 @@
 import React from "react";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from 'redux'
+import { composeWithDevTools } from '@redux-devtools/extension'
+import reduxThunk from 'redux-thunk'
 
 import { CssBaseline } from "@mui/material";
 
 import ProviderContainer from '../components/Provider/ProviderContainer'
+import reducers from '../reducers'
+
+const composeEnhancers = composeWithDevTools({ trace: false })
+const store = createStore(reducers, composeEnhancers(applyMiddleware(reduxThunk)))
 
 export default function App({ Component, pageProps }) {
 	return (
 		<React.Fragment>
-			<ProviderContainer>
-				<CssBaseline />
-				<Component {...pageProps} />
-			</ProviderContainer>
+			<Provider store={store}>
+				<ProviderContainer>
+					<CssBaseline />
+					<Component {...pageProps} />
+				</ProviderContainer>
+			</Provider>
 		</React.Fragment>
 	);
 }
