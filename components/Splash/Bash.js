b647627f49e8389f09914621cc985766d3959953
jpal91 <pallanschja@gmail.com>
2022-12-30 09:13:39 -0500

    made adjustments to terminal and bash

--- components/Splash/Bash.js
+++ components/Splash/Bash.js
@@ -1,45 +1,62 @@
-import { useRef, useEffect } from 'react'
-import Typed from 'react-typed'
+import { useRef, useEffect } from "react";
+import Typed from "react-typed";
 import Typography from "@mui/material/Typography";
-import useMediaQuery from '@mui/material/useMediaQuery'
-import { useTheme } from '@mui/material/styles'
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { useTheme } from "@mui/material/styles";
 
 const Bash = (props) => {
-    const { strings, setState, tState, output, order } = props
-    const tRef = useRef()
-    const theme = useTheme()
-    const matches = useMediaQuery(theme.breakpoints.down('md'))
+	const { strings, setState, tState, output, order } = props;
+	const tRef = useRef();
+	const theme = useTheme();
+	const matches = useMediaQuery(theme.breakpoints.down("md"));
 
-    useEffect(() => {
-        console.log(tState)
-        if (order != tState) {
-            return
-        }
-        console.log(tRef.current)
-        tRef.current.cursorBlinking = true
-		setTimeout(() => tRef.current.start(), 1000)
-    }, [tState])
+	useEffect(() => {
+		console.log(tState);
+		if (order != tState) {
+			return;
+		}
+		console.log(tRef.current);
+		tRef.current.cursorBlinking = true;
+		setTimeout(() => tRef.current.start(), 1000);
+	}, [tState]);
 
-    return (
-        <>
-        <Typography variant='h1' sx={{ display: tState < order ? 'none' : '', fontSize: matches ? '16px' : '32px', my: 1}}>
-            <span style={{color: '#76ff03'}}>$ </span>
-            <Typed 
-                typedRef={(typed) => tRef.current = typed}
-                strings={strings}
-                typeSpeed={50}
-                stopped
-                onStringTyped={ () => {
-                    setTimeout(() => {
-                        setState(tState + 1)
-                        tRef.current.cursor.hidden = true
-                    }, 2000)
-                }}
-            />
-        </Typography>
-        <Typography variant='h1' sx={{ display: tState <= order ? 'none' : '', color: 'lightBlue.200', fontSize: matches ? '16px' : '32px'}}>{output}</Typography>
-        </>
-    )
-}
+	return (
+		<>
+			<Typography
+				variant="h1"
+				sx={{
+					display: tState < order ? "none" : "",
+					fontSize: matches ? "16px" : "32px",
+					my: 1,
+				}}
+			>
+				<span style={{ color: "#76ff03" }}>$ </span>
+				<Typed
+					typedRef={(typed) => (tRef.current = typed)}
+					strings={strings}
+					typeSpeed={50}
+					stopped
+					onStringTyped={() => {
+						setTimeout(() => {
+							setState(tState + 1);
+							tRef.current.cursor.hidden = true;
+						}, 2000);
+					}}
+				/>
+			</Typography>
+			<Typography
+				variant="h1"
+				sx={{
+					display: tState <= order ? "none" : "",
+					color: "lightBlue.200",
+					fontSize: matches ? "16px" : "32px",
+                    whiteSpace: 'pre'
+				}}
+			>
+				{output.map((e) => e)}
+			</Typography>
+		</>
+	);
+};
 
-export default Bash
+export default Bash;
--- components/Splash/Terminal.js
+++ components/Splash/Terminal.js
@@ -3,9 +3,39 @@ import Card from "@mui/material/Card";
 import CardHeader from "@mui/material/CardHeader";
 import CardContent from "@mui/material/CardContent";
 
-import CircleIcon from '@mui/icons-material/Circle';
+import CircleIcon from "@mui/icons-material/Circle";
 
 import Bash from "./Bash";
+
+const base = {
+	display: "flex",
+	flexDirection: "column",
+	backgroundColor: "grey.800",
+	borderRadius: "8px",
+	border: `${1 / 16}rem solid rgba(0, 0, 0, 0.125)`,
+};
+
+const fullWidth = {
+	...base,
+	width: {
+		xs: "100%",
+		sm: "75%",
+		md: "75%",
+		lg: "75%",
+		xl: "75%",
+	},
+};
+
+const halfWidth = {
+	...base,
+	width: {
+		xs: "100%",
+		sm: "50%",
+		md: "50%",
+		lg: "50%",
+		xl: "50%",
+	},
+};
 
 const Terminal = () => {
 	const [typeState, setTypeState] = useState(0);
@@ -41,15 +71,21 @@ const Terminal = () => {
 			}}
 		>
 			<CardHeader
-				sx={{ backgroundColor: "blueGrey.300", p: 0.5, flexDirection: 'row-reverse' }}
+				sx={{
+					backgroundColor: "blueGrey.300",
+					p: 0.5,
+					flexDirection: "row-reverse",
+				}}
 				title="~:bash"
-                avatar={
-                    <>
-                    <CircleIcon sx={{ fontSize: '16px', color: 'success.focus' }}/>
-                    <CircleIcon color='warning' sx={{ fontSize: '16px' }}/>
-                    <CircleIcon color='error' sx={{ fontSize: '16px' }}/>
-                    </>
-                }
+				avatar={
+					<>
+						<CircleIcon
+							sx={{ fontSize: "16px", color: "success.focus" }}
+						/>
+						<CircleIcon color="warning" sx={{ fontSize: "16px" }} />
+						<CircleIcon color="error" sx={{ fontSize: "16px" }} />
+					</>
+				}
 				titleTypographyProps={{
 					variant: "body1",
 					sx: { display: "flex", justifyContent: "center", ml: 5 },
@@ -60,14 +96,14 @@ const Terminal = () => {
 					strings={[" cat names.txt | grep $MY_NAME | echo"]}
 					setState={setTypeState}
 					tState={typeState}
-					output={"Hello, my name is Justin"}
+					output={["Hello, my name is Justin"]}
 					order={1}
 				/>
 				<Bash
 					strings={["echo $MY_GREETING"]}
 					setState={setTypeState}
 					tState={typeState}
-					output={"Welcome to my Resume"}
+					output={["Welcome to my Resume"]}
 					order={2}
 				/>
 			</CardContent>
