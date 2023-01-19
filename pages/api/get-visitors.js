0c85e0b46747faddfd0f330c5d6855d13faf1ea8
jpal91 <pallanschja@gmail.com>
2023-01-19 12:51:26 -0500

    added API back in

--- pages/api/get-visitors.js
+++ pages/api/get-visitors.js
@@ -1,13 +1,13 @@
 import axios from "axios"
 
-// const handler = async (req, res) => {
-//     if (req.method != 'GET' || process.env.NODE_ENV == 'DEV') {
-//         return res.status(400).send('Bad request')
-//     }
+const handler = async (req, res) => {
+    if (req.method != 'GET' || process.env.NODE_ENV == 'DEV') {
+        return res.status(400).send('Bad request')
+    }
     
-//     const count = await axios.get('https://api.justinthecloud.dev/get_visitors', { headers: { 'X-API-Key': process.env.API_KEY } })
+    const count = await axios.get('https://api.justinthecloud.dev/get_visitors', { headers: { 'X-API-Key': process.env.API_KEY } })
 
-//     return res.status(200).send(count.data)
-// }
+    return res.status(200).send(count.data)
+}
 
-// export default handler
+export default handler
