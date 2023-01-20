33aec792d8107e4baacc92f8180a934c52bc01c2
jpal91 <pallanschja@gmail.com>
2023-01-20 08:55:19 -0500

    cleaned up BGProvider

--- components/Provider/BGProvider.js
+++ components/Provider/BGProvider.js
@@ -1,21 +1,6 @@
 import { connect } from "react-redux";
 import GlobalStyles from "@mui/material/GlobalStyles";
 import { useTheme } from "@mui/material/styles";
-import { keyframes } from "@emotion/react";
-
-const movingClouds = keyframes`
-	0% {
-		background-position-x: center;
-	}
-
-	50% {
-		background-position-x: 200vw;
-	}
-
-	100% {
-		background-position-x: center;
-	}
-`
 
 const BGProvider = (props) => {
 	const { section } = props;
@@ -24,31 +9,24 @@ const BGProvider = (props) => {
 	const { background, lightBlue } = theme.palette;
 
 	const sectMap = {
-		// home: background.blueGrey[200],
 		home: lightBlue[100],
-		certs: background.default
-	}
+		certs: background.default,
+	};
 
 	const globalStyles = (
 		<GlobalStyles
 			styles={{
-				body: { 
-                    backgroundColor: `${sectMap[section] && sectMap[section]} !important`,
+				body: {
+					backgroundColor: `${
+						sectMap[section] && sectMap[section]
+					} !important`,
 					backgroundImage: 'url("proj-pics/bg-cloud2.svg")',
-					backgroundRepeat: 'repeat',
-					backgroundAttachment: 'fixed',
-					backgroundSize: '70%',
-					// backgroundPosition: 'center',
-					backgroundPositionX: 'center',
-					backgroundPositionY: 'center',
-					
-					// backgroundBlendMode: 'difference'
-					// backgroundBlendMode: 'overlay',
-					// backgroundBlendMode: 'exclusion'
-					// animation: `${movingClouds} 100s linear forwards infinite`,
-					// animationPlayState: section == 'home' ? 'running' : 'paused',
-					
-                },
+					backgroundRepeat: "repeat",
+					backgroundAttachment: "fixed",
+					backgroundSize: "70%",
+					backgroundPositionX: "center",
+					backgroundPositionY: "center",
+				},
 			}}
 		/>
 	);
@@ -58,7 +36,7 @@ const BGProvider = (props) => {
 
 const mapStateToProps = (state) => {
 	return {
-		section: state.section
+		section: state.section,
 	};
 };
 
