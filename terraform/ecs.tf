e58f60c308cf1cef5799d782876ddd2236f08e6d
jpal91 <pallanschja@gmail.com>
2023-01-20 07:55:26 -0500

    added autoscaling

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -58,21 +58,28 @@ resource "aws_ecs_task_definition" "service_task_fargate" {
 }
 
 resource "aws_ecs_service" "ecs_service" {
-  name = "${var.service_name}-service"
-  cluster = aws_ecs_cluster.cluster.id
-  task_definition = aws_ecs_task_definition.service_task_fargate.arn
-  desired_count = 1
-  launch_type = "FARGATE"
+	name = "${var.service_name}-service"
+	cluster = aws_ecs_cluster.cluster.id
+	task_definition = aws_ecs_task_definition.service_task_fargate.arn
+	desired_count = 1
+	launch_type = "FARGATE"
 
-  network_configuration {
-    subnets = [aws_subnet.subnet1.id, aws_subnet.subnet2.id]
-    assign_public_ip = true
-	security_groups = [aws_security_group.ecs_tasks.id]
-  }
+	network_configuration {
+		subnets = [aws_subnet.subnet1.id, aws_subnet.subnet2.id]
+		assign_public_ip = true
+		security_groups = [aws_security_group.ecs_tasks.id]
+	}
 
- load_balancer {
-	target_group_arn = aws_alb_target_group.target_group.arn
-	container_name = var.service_name
-	container_port = 3000
- }
+	load_balancer {
+		target_group_arn = aws_alb_target_group.target_group.arn
+		container_name = var.service_name
+		container_port = 3000
+	}
+
+	lifecycle {
+		ignore_changes = [
+		  desired_count
+		]
+	}
+
 }
